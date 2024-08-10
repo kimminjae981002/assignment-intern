0dad853169b46089d18ccfefbbc1a66f7b773a6a
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2024-08-10 14:45:11 +0900

    build: databse 폴더 생성

---
+++ config/database.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("MySQL 연결에 실패했습니다:", err);
+    process.exit(1);
+  }
+  console.log("MySQL에 성공적으로 연결되었습니다.");
+});
+
+module.exports = connection;
